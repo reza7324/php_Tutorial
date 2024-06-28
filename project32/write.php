70d344c27debcd8f45950181267624ecc282233e
reza goodarzi <reza.2002.goodarzi@gmail.com>
2024-06-28 22:17:17 +0330

    php write

---
+++ project32/write.php
@@ -0,0 +1,14 @@
+<?php
+
+// $file = fopen("log.txt", "w");
+// $file = fopen("log.txt", "a");
+
+// fwrite($file, "reza goodarzi");
+
+// fclose($file);
+
+// file_put_contents("logg.txt", "php ..."); // fopen fwrite fclos
+file_put_contents("logg.txt", "php ...", FILE_APPEND);
+
+
+
